6999b4567985056ef7e88d4b1ff7ffcab44ae3f1
pierson-we <pierson.we@gmail.com>
2018-06-21 16:54:41 -0700

    added a couple of variant callers

--- code/variant_calling.py
+++ code/variant_calling.py
@@ -71,6 +71,7 @@ class filter_mutect(luigi.Task):
 		pipeline_utils.confirm_path(self.output().path)
 		cmd = [self.gatk4_location, 'FilterMutectCalls', '-V', self.input().path, '-O', self.output().path]
 		pipeline_utils.command_call(cmd, [self.output()], sleep_time=1.1)
+		self.input().remove()
 
 
 class scalpel_discovery(luigi.Task):
@@ -156,17 +157,43 @@ class freebayes(luigi.Task):
 
 
 	def output(self):
-		return luigi.LocalTarget(os.path.join(self.vcf_path, 'scalpel', 'variants.db.dir'))
+		return luigi.LocalTarget(os.path.join(self.vcf_path, self.case + '_freebayes' + '.vcf'))
 	
 	def run(self):
 		pipeline_utils.confirm_path(self.output().path)
 		if self.matched_n:
-			cmd = ['./scalpel-0.5.4/scalpel-discovery', '--somatic', '--normal', self.input()[1][0].path, '--tumor', self.input()[0][0].path, '--bed', self.library_bed, '--ref', self.fasta_file, '--two-pass', '--dir', os.path.join(self.vcf_path, 'scalpel'), '--numprocs', str(self.max_threads)]
+			cmd = ['./packages/freebayes/scripts/freebayes-parallel', '<(fasta_generate_regions.py %s.fai 100000)' % self.fasta_file, self.max_threads, '-f', self.fasta_file, '-t', self.library_bed, '--pooled-continuous', '--pooled-discrete', '-F', '0.01', '-C', '2', self.input()[0][0].path, self.input()[1][0].path, '>', os.path.join(self.vcf_path, 'freebayes')]
 		else:
-			cmd = ['./scalpel-0.5.4/scalpel-discovery', '--single', '--bam', self.input()[0][0].path, '--bed', self.library_bed, '--ref', self.fasta_file, '--dir', os.path.join(self.vcf_path, 'scalpel'), '--numprocs', str(self.max_threads)]
+			cmd = ['./packages/freebayes/scripts/freebayes-parallel', '<(fasta_generate_regions.py %s.fai 100000)' % self.fasta_file, self.max_threads, '-f', self.fasta_file, '-t', self.library_bed, '--pooled-continuous', '--pooled-discrete', '-F', '0.01', '-C', '2', self.input()[0][0].path, '>', os.path.join(self.vcf_path, 'freebayes')]
 		pipeline_utils.command_call(cmd, [self.output()])
-# 'VarDict': {'tumor_only': 'AF_THR="0.01" \
-# vardict -G %s -f $AF_THR -N %s -b %s -c 1 -S 2 -E 3 -g 4 -x 100 %s | teststrandbias.R | var2vcf_valid.pl -N %s -E -f $AF_THR' % (self.fasta_file, self.case + 'T', tumor_bam, self.library_bed, self.case + 'T'),
-# 			'matched_n': 'AF_THR="0.01" \
-# vardict -G %s -f $AF_THR -N %s -b "%s|%s" -c 1 -S 2 -E 3 -g 4 -x 100 %s | testsomatic.R | var2vcf_paired.pl -N "%s|%s" -f $AF_THR' % (self.fasta_file, self.case + 'T', tumor_bam, normal_bam, self.library_bed, self.case + 'T', self.case + 'N'),},
-# 		
+
+class vardict(luigi.Task):
+	max_threads = luigi.IntParameter()
+	matched_n = luigi.Parameter()
+	project_dir = luigi.Parameter()
+
+	case = luigi.Parameter()
+	tumor = luigi.Parameter()
+	matched_n = luigi.Parameter()
+	vcf_path = luigi.Parameter()
+
+	library_bed = luigi.Parameter()
+	fasta_file = luigi.Parameter()
+
+	def requires(self):
+		if self.matched_n != '':
+			return [bam_processing.recalibrated_bam(sample=self.case + '_T', fastq_file=self.tumor, project_dir=self.project_dir, max_threads=int(self.max_threads/2)), bam_processing.recalibrated_bam(sample=self.case + '_N', fastq_file=self.matched_n, project_dir=self.project_dir, max_threads=int(self.max_threads/2))]
+		else:
+			return [bam_processing.recalibrated_bam(sample=self.case + '_T', fastq_file=self.tumor, project_dir=self.project_dir, max_threads=self.max_threads)]
+
+
+	def output(self):
+		return luigi.LocalTarget(os.path.join(self.vcf_path, self.case + '_freebayes' + '.vcf'))
+	
+	def run(self):
+		pipeline_utils.confirm_path(self.output().path)
+		if self.matched_n:
+			cmd = ['./packagers//VarDictJava/build/install/VarDict/bin/VarDict', '-G', self.fasta_file, '-f', '0.01', '-N', self.case + '_T', '-b', '"%s|%s"' % (self.input()[0][0].path, self.input()[1][0].path), '-z', '-F', '-c', '1', '-S', '2', '-E', '3', '-g', '4', self.library_bed, '|', './packages/VarDictJava/VarDict/testsomatic.R', '|', './packages/VarDictJava/VarDict/var2vcf_paired.pl', '-N', '"%s|%s"' % (self.case + '_T', self.case + '_N'), '-f', '0.01', '>%s' % os.path.join(self.vcf_path, 'vardict')]
+		else:
+			cmd = ['./packages//VarDictJava/build/install/VarDict/bin/VarDict', '-G', self.fasta_file, '-f', '0.01', '-N', self.case + '_T', '-b', self.input()[0][0].path, '-z', '-c', '1', '-S', '2', '-E', '3', '-g', '4', self.library_bed, '|', './packages/VarDictJava/VarDict/teststrandbias.R', '|', './packages/VarDictJava/VarDict/var2vcf_valid.pl', '-N', self.case + '_T', 'E', '-f', '0.01', '>%s' % os.path.join(self.vcf_path, 'vardict')]
+		pipeline_utils.command_call(cmd, [self.output()])
