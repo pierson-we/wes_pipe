fe181084a0881e6e39c0480b02b9d9b34c19af09
pierson-we <pierson.we@gmail.com>
2018-06-22 14:38:34 -0700

    troubleshoot

--- code/pipeline_utils.py
+++ code/pipeline_utils.py
@@ -16,35 +16,35 @@ def confirm_path(file):
 				raise
 
 def command_call(cmd, outputs, cwd=os.getcwd(), threads_needed=1, sleep_time=1):
-	try:
-		# wait_time = random.uniform(0,0.3)
-		# print(wait_time)
-		# time.sleep(wait_time)
-		# print('max threads: %s' % global_max_threads)
-		# print('thread count: %s' % get_thread_count(thread_file))
-		# # while global_max_threads - thread_count < threads_needed:
-			
-		# # 	time.sleep(sleep_time)
-		# # thread_count += threads_needed
-		# # print('new thread count: %s' % thread_count)
-		print('\n\n' + ' '.join(cmd) + '\n\n')
+	# try:
+	# wait_time = random.uniform(0,0.3)
+	# print(wait_time)
+	# time.sleep(wait_time)
+	# print('max threads: %s' % global_max_threads)
+	# print('thread count: %s' % get_thread_count(thread_file))
+	# # while global_max_threads - thread_count < threads_needed:
 		
-		while not add_thread_count(thread_file, threads_needed):
-			print('waiting for godot...')
-			time.sleep(sleep_time)
-		subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		while not sub_thread_count(thread_file, threads_needed):
-			# print('waiting for godot...')
-			time.sleep(sleep_time)
-		# thread_count -= threads_needed
-	except Exception as e:
-		os.chdir(cwd)
-		for output in outputs:
-			output.remove()
-			# print('file_removed')
-		# time.sleep(random.uniform(0,0.3))
-		os.remove(thread_file)
-		raise e
+	# # 	time.sleep(sleep_time)
+	# # thread_count += threads_needed
+	# # print('new thread count: %s' % thread_count)
+	print('\n\n' + ' '.join(cmd) + '\n\n')
+	
+	while not add_thread_count(thread_file, threads_needed):
+		print('waiting for godot...')
+		time.sleep(sleep_time)
+	subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	while not sub_thread_count(thread_file, threads_needed):
+		# print('waiting for godot...')
+		time.sleep(sleep_time)
+	# thread_count -= threads_needed
+	# except Exception as e:
+	# 	os.chdir(cwd)
+	# 	for output in outputs:
+	# 		output.remove()
+	# 		# print('file_removed')
+	# 	# time.sleep(random.uniform(0,0.3))
+	# 	os.remove(thread_file)
+	# 	raise e
 
 
 def init_thread_file(thread_file):
