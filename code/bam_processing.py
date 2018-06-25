cec3c027f9e9bdee9122fdc85250a885b07a2add
pierson-we <pierson.we@gmail.com>
2018-06-25 14:49:03 -0700

    error handling - still figuring it out

--- code/bam_processing.py
+++ code/bam_processing.py
@@ -105,19 +105,20 @@ class bowtie(luigi.Task):
 		return luigi.LocalTarget(os.path.join(os.path.join(*self.fasta_file.split('/')[:-1]), 'index', self.sample + '_raw.bam'))
 
 	def run(self):
-		# try:
-		cwd = os.getcwd()
-		# print(cwd)
+		try:
+			cwd = os.getcwd()
+			# print(cwd)
 
-		# os.chdir(os.path.join(os.path.join(*self.fasta_file.split('/')[:-1]), 'index'))
-		os.chdir(os.path.join(self.fasta_dir, 'index'))
-		# print(os.getcwd())
-		# cmd = [os.path.join(cwd, self.bowtie_location, 'bowtie2'), '-x', self.base_name, '--threads=%s' % self.max_threads, '-U', self.fastq_file, '-S', self.sample + '_raw.sam']
-		cmd = [os.path.join(cwd, self.bowtie_location, 'bowtie2'), '-x', self.base_name, '--threads=%s' % self.max_threads, '-1', self.fastq_file.split('\t')[0], '-2', self.fastq_file.split('\t')[1], '-S', self.sample + '_raw.bam']
-		pipeline_utils.command_call(cmd, [self.output()], cwd=cwd, threads_needed=self.max_threads, sleep_time=0.2)
+			# os.chdir(os.path.join(os.path.join(*self.fasta_file.split('/')[:-1]), 'index'))
+			os.chdir(os.path.join(self.fasta_dir, 'index'))
+			# print(os.getcwd())
+			# cmd = [os.path.join(cwd, self.bowtie_location, 'bowtie2'), '-x', self.base_name, '--threads=%s' % self.max_threads, '-U', self.fastq_file, '-S', self.sample + '_raw.sam']
+			cmd = [os.path.join(cwd, self.bowtie_location, 'bowtie2'), '-x', self.base_name, '--threads=%s' % self.max_threads, '-1', self.fastq_file.split('\t')[0], '-2', self.fastq_file.split('\t')[1], '-S', self.sample + '_raw.bam']
+			pipeline_utils.command_call(cmd, [self.output()], cwd=cwd, threads_needed=self.max_threads, sleep_time=0.2)
 
-		os.chdir(cwd)
-		# except KeyboardInterrupt:
+			os.chdir(cwd)
+		except KeyboardInterrupt:
+			pipeline_utils.error_handling(KeyboardInterrupt)
 		# 	os.chdir(cwd)
 		# 	self.output().remove()
 		# 	sys.exit()
