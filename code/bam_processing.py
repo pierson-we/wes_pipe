8f66ff5132be5ae02c744dd609ea3eb21dc1d46e
pierson-we <pierson.we@gmail.com>
2018-07-09 01:06:34 -0700

    bowtie reference

--- code/bam_processing.py
+++ code/bam_processing.py
@@ -167,7 +167,7 @@ class bowtie(luigi.Task):
 		# os.chdir(os.path.join(self.fasta_dir, 'index'))
 		# print(os.getcwd())
 		# cmd = [os.path.join(cwd, self.bowtie_location, 'bowtie2'), '-x', self.base_name, '--threads=%s' % self.max_threads, '-U', self.fastq_file, '-S', self.sample + '_raw.sam']
-		cmd = [self.bowtie_location, '-x', os.path.join(self.fasta_dir, 'index', self.base_name), '-1', self.input()[-1][0][0].path, '-2', self.input()[-1][1][0].path, '-p', self.max_threads, '|', self.samtools_location, 'view', '-b', '-', '>', self.output().path]
+		cmd = [self.bowtie_location, '-x', os.path.join(self.fasta_dir, 'index', self.base_name), '-1', self.input()[-1][1][0].path, '-2', self.input()[-1][2][0].path, '-p', self.max_threads, '|', self.samtools_location, 'view', '-b', '-', '>', self.output().path]
 		pipeline_utils.command_call(cmd, [self.output()], cwd=os.getcwd(), threads_needed=self.max_threads, sleep_time=0.2)
 
 		# os.chdir(global_vars.cwd)
