37dbe2a9c3e90e897b279ff67523d153d65a28bb
pierson-we <pierson.we@gmail.com>
2018-07-10 17:41:20 -0700

    bqsr

--- code/bam_processing.py
+++ code/bam_processing.py
@@ -364,7 +364,7 @@ class recalibrated_bam(luigi.Task):
 	def run(self):
 		for output in self.output():
 			pipeline_utils.confirm_path(output.path)
-		cmd = [self.gatk4_location, 'ApplyBQSR', '-R', self.fasta_file, '-I', self.input()[0].path, '--bqsr-recal-file', self.input()[2].path, '-O',  self.output()[0].path]
+		cmd = [self.gatk4_location, '--java-options', '"-Xmx8g -XX:+UseSerialGC"', 'ApplyBQSR', '-R', self.fasta_file, '-I', self.input()[0].path, '--bqsr-recal-file', self.input()[2].path, '-O',  self.output()[0].path]
 		pipeline_utils.command_call(cmd, self.output(), sleep_time=0.9)
 		for input_file in self.input():
 			input_file.remove()
