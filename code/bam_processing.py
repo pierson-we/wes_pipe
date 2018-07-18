d62d89c9453a04d3091590030e113e55d67b191d
pierson-we <pierson.we@gmail.com>
2018-07-18 09:21:02 -0700

    cnvkit function call

--- code/bam_processing.py
+++ code/bam_processing.py
@@ -502,7 +502,7 @@ class cases(luigi.Task):
 	def requires(self):
 		# return [aggregate_variants(case=case, tumor=self.sample_dict[case]['T'], matched_n=self.sample_dict[case]['N'], project_dir=self.project_dir, max_threads=self.sample_threads, case_dict=self.sample_dict) for case in self.sample_dict]
 
-		return [variant_analysis.vep(case=case, tumor=self.sample_dict[case]['T'], matched_n=self.sample_dict[case]['N'], project_dir=self.project_dir, max_threads=self.sample_threads, case_dict=self.sample_dict) for case in self.sample_dict] + [variant_calling.cnvkit(case_dict=self.sample_dict, project_dir=self.project_dir, max_threads=self.sample_threads)]
+		return [variant_analysis.vep(case=case, tumor=self.sample_dict[case]['T'], matched_n=self.sample_dict[case]['N'], project_dir=self.project_dir, max_threads=self.sample_threads, case_dict=self.sample_dict) for case in self.sample_dict] + [cnvkit(case_dict=self.sample_dict, project_dir=self.project_dir, max_threads=self.sample_threads)]
 		# # global global_max_threads, thread_count
 		
 		# global_vars.global_max_threads = self.max_threads
