bf31fdfb39ccb641b45e6cf468a723ea3a0d9c00
pierson-we <pierson.we@gmail.com>
2018-07-19 15:07:30 -0700

    fpfilter params

--- code/variant_analysis.py
+++ code/variant_analysis.py
@@ -31,7 +31,8 @@ class vep(luigi.Task):
 		return [luigi.LocalTarget(os.path.join(vcf_path, self.case + '_vep.vcf')), luigi.LocalTarget(os.path.join(vcf_path, self.case + '_vep.vcf_summary.html'))]
 	
 	def run(self):
-		pipeline_utils.confirm_path(self.output().path)
+		for output in self.output():
+			pipeline_utils.confirm_path(output.path)
 		cmd = ['./packages/ensembl-vep/vep', '-i', self.input()[0].path, '-o', self.output().path, '--fasta', self.fasta_file, '--cache', '--dir_cache', './packages/ensembl-vep/cache', '--protein', '--symbol', '--hgvs', '--force_overwrite', '--check_existing', '--offline', '--buffer_size', '2500']
 		pipeline_utils.command_call(cmd, self.output())
 
@@ -71,6 +72,36 @@ class fpfilter(luigi.Task):
 		cmd = ['./packages/fpfilter/fpfilter.pl', '--var-file', self.input()[0].path, '--readcount-file', snvs_readcount, '--output-file', self.output().path]
 		pipeline_utils.command_call(cmd, [self.output()])
 
+# class msings_baseline(luigi.Task):
+# 	max_threads = luigi.IntParameter()
+# 	project_dir = luigi.Parameter()
+
+# 	vcf_path = luigi.Parameter()
+# 	case = luigi.Parameter()
+# 	tumor = luigi.Parameter()
+# 	matched_n = luigi.Parameter()
+
+# 	fasta_file = luigi.Parameter()
+
+# 	def requires(self):
+# 		return [bam_processing.recalibrated_bam(sample=self.case + '_T', fastq_file=self.tumor, project_dir=self.project_dir, max_threads=self.max_threads)]
+
+# 	def output(self):
+# 		return [luigi.LocalTarget(os.path.join(self.project_dir, 'output', 'msings', 'normal_bams.txt'))]
+	
+# 	def run(self):
+# 		for output in self.output():
+# 			pipeline_utils.confirm_path(output.path)
+# 		cmd = ['./packages/msings/scripts/create_intervals.sh', './packages/MANTIS/b37_exome_microsatellites.bed']
+# 		pipeline_utils.command_call(cmd, self.output())
+
+# 		normal_bams_file = os.path.join(self.project_dir, 'output', 'msings', 'normal_bams.txt')
+# 		with open(normal_bams_file, 'w') as f:
+# 			normal_bams_list = [os.path.join(self.project_dir, 'output', self.case, 'alignment', case_name + '_N_recalibrated.bam') for case_name in self.case_dict if self.case_dict[case_name]['N'] != '']
+# 			f.write('\n'.join(normal_bams_list))
+# 		cmd = ['./packages/msings/scripts/create_baseline.sh', normal_bams_file, './packages/MANTIS/b37_exome_microsatellites.intervals', './packages/MANTIS/b37_exome_microsatellites.bed', self.fasta_file]
+# 		pipeline_utils.command_call(cmd, self.output())
+
 class msi(luigi.Task):
 	max_threads = luigi.IntParameter()
 	project_dir = luigi.Parameter()
@@ -92,10 +123,13 @@ class msi(luigi.Task):
 		return [luigi.LocalTarget(os.path.join(self.vcf_path, self.case + '_msi.txt')), luigi.LocalTarget(os.path.join(self.vcf_path, self.case + '_msi.txt.status')), luigi.LocalTarget(os.path.join(self.vcf_path, self.case + '_msi.kmer_counts.txt')), luigi.LocalTarget(os.path.join(self.vcf_path, self.case + '_msi.kmer_counts_filtered.txt'))]
 	
 	def run(self):
-		pipeline_utils.confirm_path(self.output().path)
+		for output in self.output():
+			pipeline_utils.confirm_path(output.path)
 		if self.matched_n != '':
 			cmd = ['python3', './packages/MANTIS/mantis.py', '-b', './packages/MANTIS/b37_exome_microsatellites.bed', '--genome', self.fasta_file, '-t', self.input()[0][0].path, '-n', self.input()[1][0].path, '--threads', self.max_threads, '-mrq', '20.0', '-mlq', '25.0', '-mlc', '20', '-mrr', '1', '-o', self.output().path]
 			pipeline_utils.command_call(cmd, self.output(), threads_needed=self.max_threads)
 		else:
+			# cmd = ['./packages/msings/scripts/run_msings.sh', self.input()[0].path, './packages/MANTIS/b37_exome_microsatellites.intervals', './packages/MANTIS/b37_exome_microsatellites.bed', self.fasta_file]
+			# pipeline_utils.command_call(cmd, self.output())
 			cmd = ['echo', '"mSINGS still needs to be set up for tumor-only samples"', '>', self.output().path] # this will be a pain to get up and going: https://bitbucket.org/uwlabmed/msings/src/8269e0e01acfc5e01d0de9d63ffc1e399996ce8a/Recommendations_for_custom_assays?at=master&fileviewer=file-view-default
 			pipeline_utils.command_call(cmd, self.output())
