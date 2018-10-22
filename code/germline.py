8a65909827b617a0c2eb8b7f75e15ce1e39cc385
pierson-we <pierson.we@gmail.com>
2018-10-21 21:39:53 -0700

    troubleshoot

--- code/germline.py
+++ code/germline.py
@@ -47,14 +47,14 @@ class consolidate_gvcfs(luigi.Task):
 
 	def run(self):
 		pipeline_utils.confirm_path(self.output().path)
-		with open('gvcf_sample_map', 'w') as f:
+		with open(os.path.join(self.project_dir, 'output', 'haplotype_caller', 'gvcf_sample_map.txt'), 'w') as f:
 			for gvcf in self.input():
 				sample = gvcf.path.split('/')[-1].split('.g.vcf')[0]
 				f.write('%s\t%s\n' % (sample, gvcf.path))
-		cmd = [self.cfg['gatk4_location'], '--java-options', '"-Xmx8g -Xms8g -XX:+UseSerialGC -Djava.io.tmpdir=%s"' % self.cfg['tmp_dir'], 'GenomicsDBImport', '--genomicsdb-workspace-path', os.path.join(self.project_dir, 'output', 'haplotype_caller', 'genomicsdb'), '--batch-size', '50', '-L', self.cfg['library_bed'], '--sample-name-map', 'gvcf_sample_map', '--TMP_DIR=%s' % self.cfg['tmp_dir'], '--reader-threads', str(self.max_threads)]
+		cmd = [self.cfg['gatk4_location'], '--java-options', '"-Xmx8g -Xms8g -XX:+UseSerialGC -Djava.io.tmpdir=%s"' % self.cfg['tmp_dir'], 'GenomicsDBImport', '--genomicsdb-workspace-path', os.path.join(self.project_dir, 'output', 'haplotype_caller', 'genomicsdb'), '--batch-size', '50', '-L', self.cfg['library_bed'], '--sample-name-map', os.path.join(self.project_dir, 'output', 'haplotype_caller', 'gvcf_sample_map.txt'), '--TMP_DIR=%s' % self.cfg['tmp_dir'], '--reader-threads', str(self.max_threads)]
 		pipeline_utils.command_call(cmd, [self.output()], threads_needed=self.max_threads)
 
-		os.remove('gvcf_sample_map')
+		os.remove(os.path.join(self.project_dir, 'output', 'haplotype_caller', 'gvcf_sample_map.txt'))
 
 class genotype_gvcfs(luigi.Task):
 	max_threads = luigi.IntParameter()
