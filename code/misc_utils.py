0fe2eac3bb54fdb6424b21d5d17f7d4b30ae9a2d
pierson-we <pierson.we@gmail.com>
2018-11-11 17:02:09 -0800

    troubleshoot

--- code/misc_utils.py
+++ code/misc_utils.py
@@ -52,7 +52,7 @@ def format_pindel(pindel_files, sample_dict, project_dir, all_samples_output):
 		
 		pindel_df_raw = pd.read_csv(file, sep='\s+', header=None)
 		pindel_df_raw.columns = [i for i in range(1, pindel_df_raw.shape[1]+1)]
-		print(pindel_df_raw.head().to_csv())
+		# print(pindel_df_raw.head().to_csv())
 		pindel_df_raw.apply(parse, axis=1, pindel_dict=pindel_dict)
 	for sample in sample_dict:
 		sample_df = pd.DataFrame(data=pindel_dict[sample], columns=['chr', 'start', 'end', 'gffTag'])
@@ -61,7 +61,7 @@ def format_pindel(pindel_files, sample_dict, project_dir, all_samples_output):
 			f.write(sample_df.to_csv(sep='\t', header=False, index=False))
 		sample_df['sample'] = sample
 		pindel_dfs.append(sample_df)
-	all_pindels = pd.concat(cnv_dfs, ignore_index=True)
+	all_pindels = pd.concat(pindel_dfs, ignore_index=True)
 	all_pindels.to_csv(all_samples_output, sep='\t', header=True, index=False)
 
 
